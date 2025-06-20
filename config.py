6cd5e26aef7cc15321a6ff82178f1fa9fddd4476
Kiran Sardar Ahmad <93036283+Kiransardarahmad19@users.noreply.github.com>
2025-06-20 18:40:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_KEY = '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'
+API_KEY = 'a-b-c'
 API_URL = 'https://openrouter.ai/api/v1/chat/completions'
 HEADERS = {
     'Authorization': f'Bearer {API_KEY}',
